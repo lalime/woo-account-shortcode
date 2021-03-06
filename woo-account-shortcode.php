488a1f771364f46f07c8a05ac7a16b24a8d43c23
Lionnel Leunkeu <lleunkeu@gmail.com>
2021-03-06 01:48:22 +0100

    Added secureDeal form shortcode and process

--- woo-account-shortcode.php
+++ woo-account-shortcode.php
@@ -122,3 +122,130 @@ function woo_custom_account_callback( $atts ) {
 
 	return $out;
 }
+
+add_shortcode( 'secudeal_form', 'render_secudeal_form' );
+
+function render_secudeal_form( $atts ) {
+   
+    $field_group_key = 'group_6041f8829e1e1';
+    $user_id = get_current_user_id();
+		
+    // enqueue
+    acf_enqueue_scripts();
+
+	ob_start();
+
+    echo '<form class="woocommerce-secudealForm edit-account" action="" method="post">';
+    // Allow $_POST data to persist across form submission attempts.
+    if( isset($_POST['acf']) ) {
+        add_filter('acf/pre_load_value', 'woo_as_filter_pre_load_value', 10, 3);
+    }
+    
+    // defaults
+    $args = array(
+        'user_id'	=> $user_id,
+        'view'		=> 'edit',
+        'el'		=> 'div',
+    );
+    
+    // vars
+    $post_id = 'user_' . $args['user_id'];
+    
+    // get field groups
+    $field_groups = acf_get_field_groups(array(
+        'user_id'	=> $args['user_id'] ? $args['user_id'] : 'new',
+        'user_form'	=> $args['view']
+    ));
+    
+    // bail early if no field groups
+    if( empty($field_groups) ) {
+        return;
+    }
+    
+    // form data
+    acf_form_data(array(
+        'screen'		=> 'user',
+        'post_id'		=> $post_id,
+        'validation'	=> ($args['view'] == 'register') ? 0 : 1
+    ));
+		
+    // elements
+    $before = '<div class="acf-user-' . $args['view'] . '-fields acf-fields -clear">';
+    $after = '</div>';
+    $fields = acf_get_fields($field_group_key);
+
+    // loop
+    foreach( $field_groups as $field_group ) {
+        
+        // vars
+        $fields = acf_get_fields( $field_group );
+        
+        // title
+        if( $field_group['style'] === 'default' ) {
+            echo '<h2>' . $field_group['title'] . '</h2>';
+        }
+        
+        // render
+        echo $before;
+        acf_render_fields( $fields, $post_id, $args['el'], $field_group['instruction_placement'] );
+        echo $after;
+    }
+    ?>
+	<p>
+		<?php // wp_nonce_field( 'save_secudeal_details', 'save-secudeal-details-nonce' ); ?>
+		<button type="submit" class="secudeal-Button button" name="save_account_details" value="<?php esc_attr_e( 'Enregistrer', 'secudeal' ); ?>"><?php esc_html_e( 'Enregistrer', 'secudeal' ); ?></button>
+		<input type="hidden" name="action" value="save_secudeal_details" />
+	</p>
+    <?php
+    echo '</form>';
+    $out = ob_get_clean();
+            
+    // actions
+    add_action('acf/input/admin_footer', 'woo_as_admin_footer', 10, 1);
+
+	return $out;
+}
+
+function woo_as_filter_pre_load_value( $null, $post_id, $field ) {
+    $field_key = $field['key'];
+    if( isset( $_POST['acf'][ $field_key ] )) {
+        return $_POST['acf'][ $field_key ];
+    }
+    return $null;
+}
+
+function woo_as_admin_footer() {
+	
+    // script
+    ?>
+    <script type="text/javascript">
+        (function($) {
+
+            // vars
+            var view = '<?php echo $this->view; ?>';
+
+            // add missing spinners
+            var $submit = $('input.button-primary');
+            if( !$submit.next('.spinner').length ) {
+                $submit.after('<span class="spinner"></span>');
+            }
+
+        })(jQuery);	
+    </script>
+    <?php
+    
+}
+
+function save_user( $user_id ) {
+		
+    // verify nonce
+    if( !acf_verify_nonce('user') ) {
+        return $user_id;
+    }
+    
+    // save
+    if( acf_validate_save_post(true) ) {
+        acf_save_post( "user_$user_id" );
+    }
+}
+
